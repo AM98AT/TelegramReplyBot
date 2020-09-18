cf89cf2f921d60cc53370b58f207acbc894b9e56
AM98AT <54725270+AM98AT@users.noreply.github.com>
2020-09-18 05:25:42 +0300

    bot

---
+++ TheConfig.py
@@ -0,0 +1,15 @@
+config2 = {
+    "apiKey": "AIzaSyCQou2GNxE8BZBohrh0iUYAhU2GQsiauug",
+    "authDomain": "telegram-bot-f6779.firebaseapp.com",
+    "databaseURL": "https://telegram-bot-f6779.firebaseio.com",
+    "storageBucket": "telegram-bot-f6779.appspot.com",
+}
+
+config = {
+    "apiKey": "AIzaSyCmNQjaN2duGUUxWF-FxF9ubawb0LSHMVk",
+    "authDomain": "reply-bot-for-telegram.firebaseapp.com",
+    "databaseURL": "https://reply-bot-for-telegram.firebaseio.com",
+    "storageBucket": "reply-bot-for-telegram.appspot.com",
+}
+
+token = "1309329858:AAGdZIqTFEhbW-oQp0IOrZVurAbwuT-S2SI"
